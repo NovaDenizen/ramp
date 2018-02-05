2d7a0ab9ee011fb96eb1725494d0d09de183c9b2
Travis CI User <travis@example.org>
2018-02-05 03:44:55 +0000

    Documentation for Aatch/ramp@27e5196

---
+++ implementors/num_integer/trait.Integer.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["num_integer"] = [];
+implementors["ramp"] = ["impl <a class=\"trait\" href=\"num_integer/trait.Integer.html\" title=\"trait num_integer::Integer\">Integer</a> for <a class=\"struct\" href=\"ramp/int/struct.Int.html\" title=\"struct ramp::int::Int\">Int</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ramp"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"ramp/int/struct.ParseIntError.html\" title=\"struct ramp::int::ParseIntError\">ParseIntError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"ramp/rational/struct.ParseRationalError.html\" title=\"struct ramp::rational::ParseRationalError\">ParseRationalError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
